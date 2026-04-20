ed4f38cb786095b7d47f8f8d6eadcf04ed380919
kakcalu13 <kevin.a.araujo@gmail.com>
2026-04-20 13:39:46 +0530

    improve resource loading and Metal integration. Guard resource paths, add Metal shader directories to HLMS, enable shader debug output, and adjust render window gamma settings for ogre-next 2.3

--- ogre2/src/Ogre2RenderEngine.cc
+++ ogre2/src/Ogre2RenderEngine.cc
@@ -601,16 +601,19 @@ void Ogre2RenderEngine::CreateResources()
     archNames.push_back(
         std::make_pair(p, "General"));
     archNames.push_back(
-        std::make_pair(p + "/materials/programs", "General"));
+        std::make_pair(common::joinPaths(p, "materials", "programs"), "General"));
     archNames.push_back(
-        std::make_pair(p + "/materials/scripts", "General"));
+        std::make_pair(common::joinPaths(p, "materials", "scripts"), "General"));
 
     for (auto aiter = archNames.begin(); aiter != archNames.end(); ++aiter)
     {
       try
       {
-        Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-            aiter->first, "FileSystem", aiter->second);
+        if (common::isDirectory(aiter->first))
+        {
+          Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+              aiter->first, "FileSystem", aiter->second);
+        }
       }
       catch(Ogre::Exception &/*_e*/)
       {
@@ -622,18 +625,32 @@ void Ogre2RenderEngine::CreateResources()
 
   // register PbsMaterial resources
   Ogre::String rootHlmsFolder = mediaPath;
+
+  // 2.0 compositor + material scripts
   Ogre::String pbsCompositorFolder = common::joinPaths(
       rootHlmsFolder, "2.0", "scripts", "Compositors");
-  Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      pbsCompositorFolder, "FileSystem", "General");
+  if (common::isDirectory(pbsCompositorFolder))
+  {
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+        pbsCompositorFolder, "FileSystem", "General");
+  }
+
   Ogre::String commonMaterialFolder = common::joinPaths(
       rootHlmsFolder, "2.0", "scripts", "materials", "Common");
-  Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      commonMaterialFolder, "FileSystem", "General");
+  if (common::isDirectory(commonMaterialFolder))
+  {
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+        commonMaterialFolder, "FileSystem", "General");
+  }
+
   Ogre::String commonGLSLMaterialFolder = common::joinPaths(
       rootHlmsFolder, "2.0", "scripts", "materials", "Common", "GLSL");
-  Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      commonGLSLMaterialFolder, "FileSystem", "General");
+  if (common::isDirectory(commonGLSLMaterialFolder))
+  {
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+        commonGLSLMaterialFolder, "FileSystem", "General");
+  }
+
 #ifdef __APPLE__
   // On macOS with Metal render system, register Metal shader resources
   Ogre::String commonMetalMaterialFolder = common::joinPaths(
@@ -719,16 +736,32 @@ void Ogre2RenderEngine::CreateResources()
       ++libraryFolderPathIt;
     }
 
+#ifdef __APPLE__
+    // Also feed the Common/Metal materials directory as a PBS HLMS library
+    // so Metal shaders under 2.0/scripts/materials/Common/Metal are visible.
+    {
+      const std::string metalLib = common::joinPaths(
+          mediaPath, "2.0", "scripts", "materials", "Common", "Metal");
+      if (common::isDirectory(metalLib))
+      {
+        Ogre::Archive *archiveLibrary =
+            archiveManager.load(metalLib, "FileSystem", true);
+        archivePbsLibraryFolders.push_back(archiveLibrary);
+      }
+    }
+#endif
+
     // Create and register
     hlmsPbs = OGRE_NEW Ogre::HlmsPbs(archivePbs, &archivePbsLibraryFolders);
     Ogre::Root::getSingleton().getHlmsManager()->registerHlms(hlmsPbs);
 
-    // disable writting debug output to disk
-    hlmsPbs->setDebugOutputPath(false, false);
+    // write generated Metal shaders to /tmp/hlms_pbs_ for debugging
+    hlmsPbs->setDebugOutputPath(true, true, "/tmp/hlms_pbs_");
   }
 }
 
 //////////////////////////////////////////////////
+
 void Ogre2RenderEngine::CreateRenderWindow()
 {
   // create dummy window
@@ -801,7 +834,7 @@ Ogre::Window *Ogre2RenderEngine::CreateOgreWindow(const std::string &_handle,
   params["contentScalingFactor"] = std::to_string(_ratio);
 
   // Ogre 2 PBS expects gamma correction
-  params["gamma"] = "true";
+  params["gamma"] = "false";
 
   if (this->useCurrentGLContext)
   {
